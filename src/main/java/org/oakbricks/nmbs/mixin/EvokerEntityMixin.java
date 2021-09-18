a9249983e5173811a3fe9ced523584faf7692f7e
OakBricks <andycruzfranco@gmail.com>
2021-09-18 09:20:06 -0500

    add more mixins

--- src/main/java/org/oakbricks/nmbs/mixin/EvokerEntityMixin.java
+++ src/main/java/org/oakbricks/nmbs/mixin/EvokerEntityMixin.java
@@ -1,7 +1,9 @@
 package org.oakbricks.nmbs.mixin;
 
 import net.minecraft.entity.mob.EvokerEntity;
+import net.minecraft.entity.mob.SpellcastingIllagerEntity;
 import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.raid.RaiderEntity;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -14,5 +16,37 @@ public class EvokerEntityMixin {
 	@Overwrite
 	public void setWololoTarget(@Nullable SheepEntity sheep) {
 		this.wololoTarget = null;
+	}
+
+	@Overwrite
+	public @Nullable SheepEntity getWololoTarget() {
+		return null;
+	}
+}
+
+@Mixin(EvokerEntity.WololoGoal.class)
+class WololoGoalMixin {
+	@Overwrite
+	public boolean canStart() {
+		return false;
+	}
+	@Overwrite
+	public boolean shouldContinue() {
+		return false;
+	}
+	@Overwrite
+	public void castSpell() {}
+	@Overwrite
+	public SpellcastingIllagerEntity.Spell getSpell() {
+		return SpellcastingIllagerEntity.Spell.NONE;
+	}
+}
+@Mixin(EvokerEntity.LookAtTargetOrWololoTarget.class)
+class LookAtTargetOrWololoTargetMixin {
+	@Overwrite
+	public void tick() {
+		if (((EvokerEntity) (Object) this).getTarget() != null) {
+			((EvokerEntity) (Object) this).getLookControl().lookAt(((EvokerEntity) (Object) this).getTarget(), (float)((EvokerEntity) (Object) this).getBodyYawSpeed(), (float)((EvokerEntity) (Object) this).getLookPitchSpeed());
+		}
 	}
 }
