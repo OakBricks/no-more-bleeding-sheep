1b5211d561fc0dbe7278d08490478f6269483a2f
OakBricks <andycruzfranco@gmail.com>
2021-09-18 10:05:24 -0500

    you got a loisence for that? also we dont depend on FAPI anymore!

--- src/main/java/org/oakbricks/nmbs/NoMoreBleedingSheep.java
+++ src/main/java/org/oakbricks/nmbs/NoMoreBleedingSheep.java
@@ -8,10 +8,6 @@ public class NoMoreBleedingSheep implements ModInitializer {
 	private static Logger LOGGER = LogManager.getLogger("No More Bleeding Sheep");
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Mod loaded!");
 	}
 }
