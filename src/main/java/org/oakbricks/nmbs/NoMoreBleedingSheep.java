2d10b1801200a5ae7dc9b78d1f8d6b88c555da68
OakBricks <andycruzfranco@gmail.com>
2021-11-21 00:06:36 -0600

    less jank!

--- src/main/java/org/oakbricks/nmbs/NoMoreBleedingSheep.java
+++ src/main/java/org/oakbricks/nmbs/NoMoreBleedingSheep.java
@@ -1,13 +1,41 @@
 package org.oakbricks.nmbs;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NoMoreBleedingSheep implements ModInitializer {
-	private static Logger LOGGER = LogManager.getLogger("No More Bleeding Sheep");
+	public static final Logger LOGGER = LogManager.getLogger("No More Bleeding Sheep");
+
+	// Load config 'config.properties', if it isn't present create one
+	// using the lambda specified as the provider.
+	public SimpleConfig CONFIG = SimpleConfig.of("no-more-bleeding-sheep").provider( this::provider ).request();
+
+	// Custom config provider, returnes the default config content
+	// if the custom provider is not specified SimpleConfig will create an empty file instead
+	private String provider( String filename ) {
+		return "#Disable the Wololo goal by settings disableWololoGoal to true\ndisableWololoGoalFromStarting=false";
+	}
+
+	public final boolean DISABLE_WOLOLO_GOAL = CONFIG.getOrDefault("disableWololoGoalFromStarting", false);
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Mod loaded!");
+		LOGGER.info("No More Bleeding Sheep loaded!");
+		List<String> configLines;
+		try {
+			configLines = Files.readAllLines(FabricLoader.getInstance().getConfigDir().resolve("no-more-bleeding-sheep.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			configLines = List.of("NULL", "NULL");
+		}
+		LOGGER.info(configLines);
+		configLines.set(1, "tele jigsaw");
+		LOGGER.info(configLines);
 	}
 }
--- src/main/java/org/oakbricks/nmbs/mixin/EvokerEntityMixin.java
+++ src/main/java/org/oakbricks/nmbs/mixin/EvokerEntityMixin.java
@@ -1,52 +1,20 @@
 package org.oakbricks.nmbs.mixin;
 
 import net.minecraft.entity.mob.EvokerEntity;
-import net.minecraft.entity.mob.SpellcastingIllagerEntity;
-import net.minecraft.entity.passive.SheepEntity;
-import net.minecraft.entity.raid.RaiderEntity;
-import org.jetbrains.annotations.Nullable;
+import org.oakbricks.nmbs.NoMoreBleedingSheep;
+import org.oakbricks.nmbs.SimpleConfig;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-
-@Mixin(EvokerEntity.class)
-public class EvokerEntityMixin {
-	@Shadow private SheepEntity wololoTarget;
-
-	@Overwrite
-	public void setWololoTarget(@Nullable SheepEntity sheep) {
-		this.wololoTarget = null;
-	}
-
-	@Overwrite
-	public @Nullable SheepEntity getWololoTarget() {
-		return null;
-	}
-}
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EvokerEntity.WololoGoal.class)
-class WololoGoalMixin {
-	@Overwrite
-	public boolean canStart() {
-		return false;
-	}
-	@Overwrite
-	public boolean shouldContinue() {
-		return false;
-	}
-	@Overwrite
-	public void castSpell() {}
-	@Overwrite
-	public SpellcastingIllagerEntity.Spell getSpell() {
-		return SpellcastingIllagerEntity.Spell.NONE;
-	}
-}
-@Mixin(EvokerEntity.LookAtTargetOrWololoTarget.class)
-class LookAtTargetOrWololoTargetMixin {
-	@Overwrite
-	public void tick() {
-		if (((EvokerEntity) (Object) this).getTarget() != null) {
-			((EvokerEntity) (Object) this).getLookControl().lookAt(((EvokerEntity) (Object) this).getTarget(), (float)((EvokerEntity) (Object) this).getBodyYawSpeed(), (float)((EvokerEntity) (Object) this).getLookPitchSpeed());
+public class EvokerEntityMixin {
+	public SimpleConfig CONFIG = new NoMoreBleedingSheep().CONFIG;
+	@Inject(at = @At("HEAD"), method = "canStart()Z", cancellable = true)
+	public void canStart(CallbackInfoReturnable<Boolean> cir) {
+		if (CONFIG.getOrDefault("disableWololoGoal", true)) {
+			cir.cancel();
 		}
 	}
 }
